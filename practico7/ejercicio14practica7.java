81ba4ff0a2dd06e37169f408fb04f5bfb58df06e
leonardosanchez119@gmail.com <leonardosanchez119@gmail.com>
2023-04-20 13:43:24 -0300

    update excersice 14

--- practico7/ejercicio14practica7.java
+++ practico7/ejercicio14practica7.java
@@ -12,6 +12,7 @@ public class ejercicio14practica7 {
     public static void main(String[] args) {
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[MAX];
+        int[] arrCopia = new int[MAX];
         int numero;
         /*
          * Hacer un programa que dado el arreglo definido y precargado, y un número
@@ -23,9 +24,14 @@ public class ejercicio14practica7 {
          * copia en este último arreglo deben comenzar desde el principio del mismo.
          */
         try {
-            cargar_arreglo_aleatorio_secuencias_int(arr);
+
             cargar_arreglo_aleatorio_secuencias_int(arr);
             imprimir_arreglo_secuencias_int(arr);
+            cargar_arreglo_con_ceros(arrCopia);
+            imprimir_arreglo_de_ceros(arrCopia);
+            System.out.println("Ingrese un numero: ");
+            numero = Integer.valueOf(entrada.readLine());
+            metodo_principal(arr, arrCopia, numero);
         } catch (Exception exc) {
             System.out.println(exc);
         }
@@ -46,10 +52,51 @@ public class ejercicio14practica7 {
     }
 
     public static void imprimir_arreglo_secuencias_int(int[] arr) {
-        System.out.print("Arreglo de secuencias int\n|");
+        System.out.println("Esto es un array de secuencias");
         for (int pos = 0; pos < MAX; pos++) {
             System.out.print(arr[pos] + "|");
         }
         System.out.print("\n");
+    }
+
+    public static void cargar_arreglo_con_ceros(int[] arr) {
+        System.out.println("Esto es un array de ceros");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+    }
+
+    public static void imprimir_arreglo_de_ceros(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "]" + "->" + arr[i]);
+        }
+    }
+
+    public static void metodo_principal(int[] arreglo, int[] arreglo2, int num) {
+
+    }
+
+    public static int encontrar_inicio_de_secuencia(int[] arr, int fin) {
+        int pos = fin;
+        while ((pos < MAX) && (arr[pos] == 0)) {
+            pos++;
+        }
+        if (pos < MAX) {
+            return pos;
+        } else {
+            return -1;
+        }
+    }
+
+    public static int encontrar_fin_de_secuencia(int[] arr, int ini) {
+        int pos = ini;
+        while ((pos < MAX) && (arr[pos] != 0)) {
+            pos++;
+        }
+        if (pos<MAX){
+            return pos-1;
+        }else{
+            return -1;
+        }
     }
 }
