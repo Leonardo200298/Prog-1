767932304cd62e463d023eac245f7d86a9e8bce2
Leonardo <leonardosanchez119@gmail.com>
2023-12-26 21:01:38 -0300

    repeticion de ejercicio 5 tp arreglos

--- practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_5Rep.java
+++ practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_5Rep.java
@@ -1,44 +1,38 @@
 package practica_final.ejercicios_de_tp.ejercicio_tp_arreglos;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-public class Ejercicio_5 {
-    /* 5. Hacer un programa que dado un arreglo de caracteres de tamaño
+ /* 5. Hacer un programa que dado un arreglo de caracteres de tamaño
     10 que se encuentra cargado, invierta el orden del contenido. Este
     intercambio no se debe realizar de manera explícita, hay que
     hacer un método que incluya una iteración de intercambio. */
+public class Ejercicio_5Rep {
     public static final int MAX = 10;
     public static void main(String[] args) {
         char [] arreglo = new char[MAX];
         cargar_arreglo(arreglo);
         imprimir_arreglo(arreglo);
-        invertir_orden(arreglo);
-        System.out.println("-------------------------------");
+        ordenar_segun_ejercicio(arreglo);
         imprimir_arreglo(arreglo);
     }
-    public static void cargar_arreglo(char[] arreglo) {
+    private static void ordenar_segun_ejercicio(char[] arreglo) {
+
+        
+    }
+    public static void cargar_arreglo(char [] arr){
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
         try {
             for (int i = 0; i < MAX; i++) {
-                System.out.println("carga el array con valores char");
-                arreglo[i] = entrada.readLine().charAt(0);
+                System.out.println("carga el arreglo:");
+                arr[i] = entrada.readLine().charAt(0);
             }
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-    public static void imprimir_arreglo(char[] arreglo) {
+
+    public static void imprimir_arreglo(char [] arreglo){
         for (int i = 0; i < MAX; i++) {
-            System.out.println("El arreglo arreglo["+i+"] -> " + arreglo[i]);
-        }
-    }
-    public static void invertir_orden(char[] arreglo){
-        char temp = ' ';
-        for (int i = 0; i < MAX/2; i++) {
-            temp = arreglo[MAX - 1 - i];
-            arreglo[MAX - 1 - i] = arreglo[i];
-            arreglo[i] = temp;
+            System.out.println("El arreglo arreglo[" + i + " ] -> " +  arreglo[i]);
         }
     }
 }
---
+++ practica_final/ejercicios_de_tp/ejercicios_tp_matrices/EjercicioM_1.java
@@ -0,0 +1,5 @@
+package practica_final.ejercicios_de_tp.ejercicios_tp_matrices;
+
+public class EjercicioM_1 {
+
+}
