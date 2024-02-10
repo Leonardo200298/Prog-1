369a687f511d706c3505678bbe7a9903954f1ec6
Leonardo200298 <leonardosanchez119@gmail.com>
2024-02-10 17:57:32 -0300

    ejercicio 17 terminado pero para mirar

--- practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_17.java
+++ practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_17.java
@@ -3,75 +3,70 @@ package practica_final.ejercicios_de_tp.ejercicio_tp_arreglos;
 import java.util.Random;
 
 public class Ejercicio_17 {
-    public static final int MAX = 40, MAXVALOR = 9, MINVALOR = 1, SEPARADOR = 0;
-    public static final double probabilidad_numero = 0.4;
+    public static final int MAX = 20;
+	public static final int MAXVALOR = 9;
+	public static final int MINVALOR = 1;
+	public static final double PROBABILIDAD_NUMERO = 0.4;
 
-    public static void main(String[] args) {
-        /*
-         * 17.Dado un arreglo de tamaño de arreglo MAX=20 de secuencias. En
-         * el caso de eliminar secuencias se debe hacer corrimiento.
-         * Hacer un programa que devuelva la posición de inicio y fin de la
-         * anteúltima secuencia de números distintos de ceros.
-         */
-        int[] arreglo = new int[MAX];
-        cargar_arreglo_aleatorio_secuencias_int(arreglo);
-        imprimir_arreglo_secuencias_int(arreglo);
-        devolver_anteultima_secuencia(arreglo);
-    }
+	public static void main(String[] args) {
+		int arreglo[] = new int[MAX];
+		cargarSecuenciasInt(arreglo);
+		imprimirArreglo(arreglo);
+		InicioAnteUltimaSecuencia(arreglo);
+	}
 
-    public static void devolver_anteultima_secuencia(int[] arreglo) {
-        int contador = 0;
-        int ini = MAX-1;
-        int finAntSec = 0;
-        int iniAntSec = 0;
-        int fin = buscar_fin(arreglo, ini);
-        while (fin>0 && contador<2) {
-            ini = buscar_inicio(arreglo, fin);
-            if (fin>0) {
-                contador++;
-                finAntSec = fin;
-                iniAntSec = ini;
-            }
-            fin = buscar_fin(arreglo, ini);
-        }
-        System.out.println("El inicio de la anteultima secuencia es " + iniAntSec +
-        " y el final de la anteultima secuencia es " + finAntSec
+	public static void InicioAnteUltimaSecuencia(int[] arreglo) {
+		int fin2;
+		int inicio2;
+		int inicio = (MAX - 1);
+		int fin1;
+		int inicio1;
+
+		fin2 = buscarFin(arreglo, inicio);
+		inicio2 = buscarInicio(arreglo, fin2);
+		fin1 = buscarFin(arreglo, inicio2);
+		inicio1 = buscarInicio(arreglo, fin1);
+		System.out.println("El inicio de la ante ultima secuencia es: " + (inicio1 + 1) + " y el fin es "
+        + fin1
         );
+	}
 
-    }
+	public static int buscarInicio(int[] arr, int pos) {
+		int i = pos;
+		while ((pos > 0 )&&(arr[i] != 0)) {
+			i--;
 
-    private static int buscar_inicio(int[] arreglo, int fin) {
-        while (fin>0 && arreglo[fin]  != SEPARADOR) {
-            fin--;
-        }
-        return fin;
-    }
+		}
 
-    public static int buscar_fin(int[] arreglo, int ini) {
-        while (ini>0 && arreglo[ini]  == SEPARADOR) {
-            ini--;
-        }
-        return ini;
-    }
+		return i;
+	}
 
-    public static void cargar_arreglo_aleatorio_secuencias_int(int[] arr) {
-        Random r = new Random();
-        arr[0] = 0;
-        arr[MAX - 1] = 0;
-        for (int pos = 1; pos < MAX - 1; pos++) {
-            if (r.nextDouble() > probabilidad_numero) {
-                arr[pos] = (r.nextInt(MAXVALOR - MINVALOR + 1) + MINVALOR);
-            } else {
-                arr[pos] = 0;
-            }
-        }
-    }
+	public static int buscarFin(int[] arr, int pos) {
+		int i = pos;
+		while ((pos > 0 )&&(arr[i] == 0)) {
+			i--;
+			
+		}
+		return i;
+	}
 
-    public static void imprimir_arreglo_secuencias_int(int[] arr) {
-        System.out.print("Arreglo de secuencias int\n|");
-        for (int pos = 0; pos < MAX; pos++) {
-            System.out.print(arr[pos] + "|");
-        }
-        System.out.print("\n");
-    }
+	public static void cargarSecuenciasInt(int[] arr) {
+		Random r = new Random();
+		arr[0] = 0;
+		arr[MAX - 1] = 0;
+		for (int pos = 1; pos < MAX - 1; pos++) {
+			if (r.nextDouble() > PROBABILIDAD_NUMERO) {
+				arr[pos] = (r.nextInt(MAXVALOR - MINVALOR + 1) + MINVALOR);
+			} else {
+				arr[pos] = 0;
+			}
+		}
+	}
+
+	public static void imprimirArreglo(int[] arr) {
+		for (int pos = 0; pos < MAX; pos++) {
+			System.out.print("[" + arr[pos] + "]");
+		}
+		System.out.println("");
+	}
 }
