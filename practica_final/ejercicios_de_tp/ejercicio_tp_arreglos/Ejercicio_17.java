f9f694c630a77140382170de0a1e3915a20e0cb8
Leonardo200298 <leonardosanchez119@gmail.com>
2024-02-11 17:29:23 -0300

    ejercicio 17, dos soluciones, la opcion comentada no escala como la solucion que esta descomentada, ya que soluciona el problema para la anteultima secuencia si, pero para arreglos de menos longitud, agregue constantes

--- practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_17.java
+++ practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_17.java
@@ -3,7 +3,8 @@ package practica_final.ejercicios_de_tp.ejercicio_tp_arreglos;
 import java.util.Random;
 
 public class Ejercicio_17 {
-    public static final int MAX = 20;
+	public static final int MAX = 20;
+	public static final int SEPARADOR = 0;
 	public static final int MAXVALOR = 9;
 	public static final int MINVALOR = 1;
 	public static final double PROBABILIDAD_NUMERO = 0.4;
@@ -16,7 +17,7 @@ public class Ejercicio_17 {
 	}
 
 	public static void InicioAnteUltimaSecuencia(int[] arreglo) {
-		int fin2;
+		/* int fin2;
 		int inicio2;
 		int inicio = (MAX - 1);
 		int fin1;
@@ -25,29 +26,41 @@ public class Ejercicio_17 {
 		fin2 = buscarFin(arreglo, inicio);
 		inicio2 = buscarInicio(arreglo, fin2);
 		fin1 = buscarFin(arreglo, inicio2);
-		inicio1 = buscarInicio(arreglo, fin1);
-		System.out.println("El inicio de la ante ultima secuencia es: " + (inicio1 + 1) + " y el fin es "
-        + fin1
+		inicio1 = buscarInicio(arreglo, fin1); */
+		 int contador = 0;
+		 int inicio = MAX - 1;
+		 int fin = 0;
+		 int iniSec = 0;
+		 int finSec = 0;
+		 fin = buscarFin(arreglo, inicio);
+		 while (fin > 0 && contador < 1){
+		 		inicio = buscarInicio(arreglo, fin);
+		 		if (fin > 0){
+		 			finSec = buscarFin(arreglo, inicio);
+		 			iniSec = buscarInicio(arreglo, finSec);	 			
+				}	 			
+				fin = buscarFin(arreglo, inicio);	 			
+				contador ++;
+		 }
+
+		System.out.println("El inicio de la ante ultima secuencia es: " + (iniSec + 1) + " y el fin es "
+        + finSec
         );
 	}
 
 	public static int buscarInicio(int[] arr, int pos) {
-		int i = pos;
-		while ((pos > 0 )&&(arr[i] != 0)) {
-			i--;
 
+		while ((pos > 0) && (arr[pos] != SEPARADOR)) {
+			pos--;
 		}
-
-		return i;
+		return pos;
 	}
 
 	public static int buscarFin(int[] arr, int pos) {
-		int i = pos;
-		while ((pos > 0 )&&(arr[i] == 0)) {
-			i--;
-			
+		while ((pos > 0) && (arr[pos] == SEPARADOR)) {
+			pos--;
 		}
-		return i;
+		return pos;
 	}
 
 	public static void cargarSecuenciasInt(int[] arr) {
