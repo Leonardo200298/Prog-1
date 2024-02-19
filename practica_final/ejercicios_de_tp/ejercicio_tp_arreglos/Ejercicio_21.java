5aff8ae8bbf1847a7579c01dd3ca360c6506f4bb
Leonardo200298 <leonardosanchez119@gmail.com>
2024-02-19 11:44:40 -0300

    agrego resolucion ejercicio 21

--- practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_21.java
+++ practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_21.java
@@ -8,13 +8,12 @@ public class Ejercicio_21 {
          * 21. Hacer un programa que elimine de un arreglo todas las
          * secuencias que tienen orden descendente entre sus elementos.
          */
-        int arreglo[] = { 0, 2, 0, 4, 2, 0, 0, 1, 2, 3, 0, 2, 0, 0, 2, 3, 6, 4, 0, 0 };
+        int arreglo[] = { 0, 2, 0, 4, 2, 0, 0, 1, 2, 3, 0, 2, 0, 2, 3, 6, 4, 4, 0, 0 };
         imprimirArreglo(arreglo);
         eliminacion_de_secuencias_ordenadas_descendentemente(arreglo);
         System.out.println("----------------");
         imprimirArreglo(arreglo);
     }
-
     public static void eliminacion_de_secuencias_ordenadas_descendentemente(int[] arreglo) {
         int fin = 0;
         int ini = buscar_inicio(arreglo, fin);
@@ -23,20 +22,17 @@ public class Ejercicio_21 {
             if (ini < MAX) {
                 boolean esSecuenciaDescendente = secuenciaDescendente(arreglo, ini, fin);
                 if (esSecuenciaDescendente) {
-                    for (int i = ini; i <= fin; i++) {
-                        
+                    for (int i = ini; i <= fin; i++) {    
                         corrimiento_izq(arreglo, ini, fin);
                     }
                 }
             }
             ini = buscar_inicio(arreglo, fin + 1);
         }
-
     }
 
     public static void corrimiento_izq(int[] arreglo, int ini, int fin) {
         while (ini<=fin) {
-            System.out.println("entre");
             arreglo[ini] = arreglo[ini+1];
             ini++;
         }
@@ -45,7 +41,7 @@ public class Ejercicio_21 {
     public static boolean secuenciaDescendente(int[] arreglo, int ini, int fin) {
         boolean ascendente = false;
         for (int i = ini; i <= fin; i++) {
-            if (arreglo[fin] > arreglo[ini] || arreglo[fin] == arreglo[ini]){
+            if ((arreglo[fin] <= arreglo[ini] || arreglo[ini] == arreglo[fin])&& fin - ini + 1 > 1){
                 ascendente = true;
             }
         }
