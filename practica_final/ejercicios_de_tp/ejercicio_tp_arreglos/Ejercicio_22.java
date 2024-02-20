b1c24b1c24a9d6e63711adb92c6cef16517a9006
Leonardo200298 <leonardosanchez119@gmail.com>
2024-02-20 12:23:53 -0300

    enunciado ejercicio 22

--- practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_22.java
+++ practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/Ejercicio_22.java
@@ -1,5 +1,9 @@
 package practica_final.ejercicios_de_tp.ejercicio_tp_arreglos;
 
 public class Ejercicio_22 {
-
+    public static void main(String[] args) {
+        /* 22. Hacer un programa que reemplace de un arreglo A todas las
+        ocurrencias de una secuencia patrón dada en un arreglo P, por la
+        secuencia contenida en el arreglo R de igual tamaño. */
+    }
 }
