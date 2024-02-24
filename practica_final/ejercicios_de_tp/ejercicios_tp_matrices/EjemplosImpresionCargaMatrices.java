e2a79b81a6bddcc4a9cf1d7dd61b12ffe648c398
Leonardo200298 <leonardosanchez119@gmail.com>
2024-02-24 14:57:52 -0300

    agrego ejemplo de carga e impresion de matrices

---
+++ practica_final/ejercicios_de_tp/ejercicios_tp_matrices/EjemplosImpresionCargaMatrices.java
@@ -0,0 +1,43 @@
+package practica_final.ejercicios_de_tp.ejercicios_tp_matrices;
+
+import java.util.Random;
+
+public class EjemplosImpresionCargaMatrices {
+    public static final int MAXFILA = 4;
+    public static final int MAXCOLUMNA = 5;
+    public static final int MINVALOR = 1;
+    public static final int MAXVALOR = 9;
+
+    public static void main (String [] args) {
+        int [][] matint = new int [MAXFILA][MAXCOLUMNA];
+        cargar_matriz(matint);
+        imprimir_matriz(matint);
+    }
+
+    public static void cargar_matriz (int [][] mat) {
+        Random r = new Random();
+        for (int fila = 0; fila < MAXFILA; fila++) {
+            cargar_arreglo(mat[fila]);
+        }
+    }
+
+    public static void cargar_arreglo(int [] arr) {
+        Random r = new Random();
+        for (int pos = 0; pos < MAXCOLUMNA; pos++) {
+            arr[pos] = (r.nextInt(MAXVALOR - MINVALOR + 1) + MINVALOR);
+        }
+    }
+
+    public static void imprimir_matriz(int [][] mat) {
+        for (int fila = 0; fila < MAXFILA; fila++) {
+            imprimir_arreglo(mat[fila]);
+        }
+    }
+
+    public static void imprimir_arreglo(int [] arr) {
+        for (int pos = 0; pos < MAXCOLUMNA; pos++) {
+            System.out.print("[" + arr[pos] + "]");
+        }
+        System.out.println("");
+    }
+}
