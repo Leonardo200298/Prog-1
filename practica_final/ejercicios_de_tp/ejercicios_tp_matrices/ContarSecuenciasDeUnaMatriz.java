ae771cc88cc68d3fd8e5a82101e2e7a6e0c3eee0
Leonardo <leonardosanchez119@gmail.com>
2024-02-25 16:02:13 -0300

    agrego ejemplo de recorrer secuencias con matrices, en este ejemplo cuento todas las secuencias de la matriz

--- practica_final/ejercicios_de_tp/ejercicios_tp_matrices/ContarSecuenciasDeUnaMatriz.java
+++ practica_final/ejercicios_de_tp/ejercicios_tp_matrices/ContarSecuenciasDeUnaMatriz.java
@@ -2,21 +2,55 @@ package practica_final.ejercicios_de_tp.ejercicios_tp_matrices;
 
 import java.util.Random;
 
-public class EjemploImpresionCargaMatricesImpresionMatricesAleatoria {
+public class ContarSecuenciasDeUnaMatriz {
+    public static final int SEPARADOR = 0;
     public static final int MAXFILA = 4;
     public static final int MAXCOLUMNA = 20;
     public static final int MAXVALOR = 9;
     public static final int MINVALOR = 1;
     public static final double PROBABILIDAD = 0.4;
-
     public static void main(String[] args) {
         int [][] matint = new int[MAXFILA][MAXCOLUMNA];
         cargar_matriz_secuencias_int(matint);
         imprimir_matriz_int(matint);
-        
+        iterar_matrices_como_arreglo(matint);
     }
+    public static void iterar_matrices_como_arreglo(int[][] mat) {
+        int contador = 0;
+        for (int fila = 0; fila < MAXFILA; fila++) {
 
-    
+            contador += contar_secuencias_en_matriz(mat[fila]);
+        }
+        System.out.println("La cantidad de secuencias es: " + contador);
+    }
+    public static int contar_secuencias_en_matriz(int[] matint) {
+        int fin = 0;
+        int contador = 0;
+        int ini = buscar_ini(matint,fin);
+        while (ini < MAXCOLUMNA) {
+            fin = buscar_fin(matint,ini);
+            if (ini < MAXCOLUMNA) {
+                int longitudSecuencia = fin-ini+1;
+                if (longitudSecuencia > 0) {
+                    contador++;
+                }
+            }
+            ini = buscar_ini(matint, fin + 1);
+        }
+        return contador;
+    }
+    private static int buscar_fin(int[] matint, int ini) {
+        while (ini < MAXCOLUMNA && matint[ini] != SEPARADOR) {
+            ini++;
+        }
+        return ini - 1;
+    }
+    public static int buscar_ini(int[] matint, int fin) {
+        while (fin < MAXCOLUMNA && matint[fin] == SEPARADOR) {
+            fin++;
+        }
+        return fin;
+    }
     public static void cargar_matriz_secuencias_int(int [][] mat) {
         for (int fila = 0; fila < MAXFILA; fila++) {
             cargar_arreglo_secuencias_int(mat[fila]);
