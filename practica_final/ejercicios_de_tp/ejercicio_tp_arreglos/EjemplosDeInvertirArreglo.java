88ec1d112f0434e0a060aa46c394302142fc7696
Leonardo200298 <leonardosanchez119@gmail.com>
2024-03-02 16:09:32 -0300

    ambas soluciones para invertir arreglos son validas

--- practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/EjemplosDeInvertirArreglo.java
+++ practica_final/ejercicios_de_tp/ejercicio_tp_arreglos/EjemplosDeInvertirArreglo.java
@@ -2,11 +2,14 @@ package practica_final.ejercicios_de_tp.ejercicio_tp_arreglos;
 
 public class EjemplosDeInvertirArreglo {
     public static final int MAX = 5;
+
     public static void main(String[] args) {
-        int []arreglo = {1,2,5,1,5};
-        System.out.println("-----------mostrar arreglo con solucion catedra-----------");
-        invetirArregloSolucionCatedra(arreglo);
+        int[] arreglo = { 1, 2, 5, 1, 5 };
+        System.out.println("----------arreglo----------");
         mostrar(arreglo);
+       /*  System.out.println("-----------mostrar arreglo con solucion catedra-----------");
+        invetirArregloSolucionCatedra(arreglo);
+        mostrar(arreglo); */
         System.out.println("-----------mostrar arreglo con solucion mia-----------");
         invertirArregloSolucionMia(arreglo);
         mostrar(arreglo);
@@ -15,15 +18,27 @@ public class EjemplosDeInvertirArreglo {
     public static void invertirArregloSolucionMia(int[] arreglo) {
         int pos = 0;
         int temporal;
-        while (pos < MAX/2) {
+        while (pos < (MAX - 1) / 2) {
             temporal = arreglo[MAX - 1 - pos];
+            arreglo[MAX - 1 - pos] = arreglo[pos];
+            arreglo[pos] = temporal;
+            pos ++;
         }
     }
-    
-    
-    public static void invetirArregloSolucionCatedra(int[] arreglo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'invetirArregloSolucionCatedra'");
+
+    public static void invetirArregloSolucionCatedra(int[] arr) {
+        int aux;
+        int ini = 0;
+        int fin = MAX - 1;
+        while (ini < fin) {
+            aux = arr[ini];
+            arr[ini] = arr[fin];
+            arr[fin] = aux;
+            ini++;
+            fin--;
+            System.out.println("Esto es ini " + ini);
+            System.out.println("Esto es fin " + fin);
+        }
     }
 
     public static void mostrar(int[] arreglo) {
