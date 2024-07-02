b32104ecbd38c7061c86b7275f7feb32e31e3ddd
Leonardo <leonardosanchez119@gmail.com>
2024-07-02 13:18:48 -0300

    agrego esqueleto de ejercicio de prefinal 2024

---
+++ practica_final/examenes/Recuperatorio2024.java
@@ -0,0 +1,9 @@
+public class Recuperatorio2024 {
+    public static final int M = 20;
+    public static final int N = 3;
+    public static void main(String[] args) {
+        char [][] V = {{'x','t','a','C','M','O','t','a','a','t','O','C','t','t','a','O','M','C','t','x'},
+        {'x','r','r','r','C','C','O','O','r','r','C','r','G','G','G','r','r','x','x','x'},
+        {'x','m','G','m','h','h','L','G','G','O','h','h','m','m','O','B','C','M','x','x'}};
+    }
+}
